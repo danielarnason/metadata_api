b7f23d0056f6461fc6b3ec5084e02c82580a7c47
Daníel Örn Árnason <danielarnason85@gmail.com>
2018-10-29 10:56:40 +0100

    Simple api virker

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+
+api = Api(app)
+
+metadata = {
+    'hello': 'WORLD!!!'
+}
+
+class MetadataList(Resource):
+    def get(self):
+        return metadata
+
+api.add_resource(MetadataList, '/metadata')
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/danielarnason'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
