abb53b8652fcf3c17b265e40229a4f0a2717b721
Daníel Örn Árnason <danielarnason85@gmail.com>
2018-10-29 11:11:12 +0100

    Simple api fra model virker

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 api = Api(app)
 
-metadata = {
-    'hello': 'WORLD!!!'
-}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/danielarnason'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import *
 
 class MetadataList(Resource):
     def get(self):
-        return metadata
+        metadata = Metadata.query.all()
+        return [{'schema': i.schema, 'table': i.tablename} for i in metadata]
 
 api.add_resource(MetadataList, '/metadata')
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/danielarnason'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
