af8a9e66f1bac9c768d210ea74caf3b04062b87b
danielarnason <danielarnason85@gmail.com>
2018-10-30 13:48:40 +0100

    Kører nu med sqlite database og flask-migrate

--- app.py
+++ app.py
@@ -2,16 +2,22 @@ from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://postgres@localhost:5432/danielarnason"
+] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 api = Api(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+migrate = Migrate(app, db)
 
 
 class Metadata(db.Model):
